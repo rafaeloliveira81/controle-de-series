13fa206719a594defbff6492ea1f6d4b8ddc7768
Rafael Pereira de Oliveira <rafael.oliveira4@alunos.unis.edu.br>
2021-09-22 22:15:13 -0300

    Integração Laravel + Vue JS
    Limpeza dos arquivos que não serão utilizados
    Refatoração das rotas para api de séries
    Configuração do Vue para renderizar com Laravel

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello Laravel + Vue
+        Template do Vue!
+    </div>
+</template>
+
+<script charset="utf-8">
+export default {
+}
+    
+</script>
+<style scoped>
+</style>
+        
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+        
+        Route::get('/v1/serie', [SeriesController::class, 'index']);
+
+    Route::prefix('/v1/serie')->group(function () {
+        Route::post('/v1/serie', [SeriesController::class, 'store']);
+        Route::put('/v1/serie', [SeriesController::class, 'update']);
+        Route::delete('/v1/serie', [SeriesController::class, 'destroy']);
+    });
