d46eb890435b31b8e1ee189ee2395188abdba3cb
Marcus Vinícius Magalhães de Freitas <marcus.freitas@alunos.unis.edu.br>
2021-10-13 19:46:04 -0300

    Alterações

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,14 +1,29 @@
 <template>
-    <div>
-        Este é um Template do Vue!
+    <div class="series-container">
+       <div class="heading">
+           <h2 id="title">Séries</h2>
+           <!--- Todo botão do formulário --->
+       </div> 
+       <list-view :series="series"/>   
     </div>
 </template>
 
 <script charset="utf-8">
+import listView from './listView.vue'
+
 export default {
+  components: { listView },
+    Components: {
+        listView
+    },
+
+    data: function() {
+        return {
+            series: []
+        }
+    }
 }
     
 </script>
 <style scoped>
 </style>
-        
