6c04552cd576f926e96fae1869dc2d1aaf2b5eb7
Rafael Pereira de Oliveira <rafael.oliveira4@alunos.unis.edu.br>
2021-11-24 20:47:07 -0300

    Implementação do backend
    Definição das migrations, configurados relacionamentos,
    criação das rotas para temporadas e implementado rota
    para listar todas as temporadas.

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Serie extends Model
 {
     use HasFactory;
     protected $fillable = ['nome', 'categoria', 'streaming', 'status'];
+
+    public function temporadas()
+    {
+        return $this->HasMany(Temporada::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SeriesController;
+use App\Http\Controllers\TemporadasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,11 +11,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
         
     Route::get('/v1/series', [SeriesController::class, 'index']);
-
     Route::prefix('/v1/serie')->group(function () {
         Route::post('/', [SeriesController::class, 'store']);
         Route::get('/{id}', [SeriesController::class, 'show']);
         Route::patch('/{id}', [SeriesController::class, 'update']);
         Route::put('/status/{id}', [SeriesController::class, 'status']);
         Route::delete('/{id}', [SeriesController::class, 'destroy']);
+    });
+
+    Route::get('/v1/temporadas', [TemporadasController::class, 'index']);
+    Route::prefix('v1/temporada')->group(function () {
+        Route::post('/', [TemporadasController::class, 'store']);
+        Route::get('/{id}', [TemporadasController::class, 'show']);
+        Route::patch('/{id}', [TemporadasController::class, 'update']);
+        Route::delete('/{id}', [TemporadasController::class, 'destroy']);
     });
